6927fd66c6ffe08b1d2c1f23f31ad34feaeef203
Viniciusovski <vini98.2012@gmail.com>
2022-02-17 12:25:22 -0300

    Fundamentos de Javascript

--- Javascript/01 - fundamentos/ternario.js
+++ Javascript/01 - fundamentos/ternario.js
@@ -1,4 +1,9 @@
 const resultado = nota => nota >= 7 ? 'Aprovado' : 'Reprovado'
+/*
+* Verdadeiro = ?
+* Falso = :
+* Se nota for maior ou igual a 7, retorna Aprovado, senão retorna Reprovado
+*/
 
 console.log(resultado(7.1))
 console.log(resultado(6.7))
--- Javascript/01 - fundamentos/uniarios.js
+++ Javascript/01 - fundamentos/uniarios.js
@@ -6,5 +6,9 @@ console.log(num1)
 --num1
 console.log(num1)
 
-console.log(++num1 === num2--)
+console.log(++num1 === num2--) // A posição dos sinais altera a ordem da execução
 console.log(num1 === num2)
+
+/*
+*Não coloque incremento em comparações
+*/
