9d878102b35d584d22df4ff99ba955eb4a87a929
Viniciusovski <vini98.2012@gmail.com>
2022-02-18 14:19:22 -0300

    Estruturas de Controle

--- Javascript/02 - Estruturas de Controle/breakContinue.js
+++ Javascript/02 - Estruturas de Controle/breakContinue.js
@@ -2,14 +2,14 @@ const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 for (x in nums) {
     if (x == 5) {
-        break
+        break // Interrompe a repetição e sai do laço
     }
     console.log(`${x} = ${nums[x]}`)
 }
 
 for (y in nums) {
     if (y == 5) {
-        continue
+        continue // interrompe a repetição e continua no proximo elemento
     }
     console.log(`${y} = ${nums[y]}`)
 }
